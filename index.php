1bd58d84e6eedf1bafc391af93bfaa94661f8f2c
MalikAliShikra <malikalishikra1@gmail.com>
2017-04-22 14:36:24 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,387 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html>
+
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>-=[ ISRAR AHMAD ]=-</title>
+
+<link rel="stylesheet" type="text/css" href="fathul.css" media="all,handheld"/>
+
+<link rel="shortcut icon" href="https://www.facebook.com/favicon.ico">
+
+<head>
+
+
+<?php session_start();
+
+	error_reporting(0);
+
+	$bot=new bot();
+
+	class bot{
+
+public function getGr($as,$bs){
+
+	$ar=array(
+
+	'graph',
+
+	'fb',
+
+	'me'
+);
+
+$im='https://'.implode('.',$ar);
+
+	return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+	$ar=array(
+	
+	'access_token' => $tk,
+);
+	
+	if($uh){
+
+	$else=array_merge($ar,$uh);
+
+	}else{
+
+	$else=$ar;
+}
+	foreach($else as $b => $c){
+	
+	$olamba[]=$b.'='.$c;
+}
+	$true='?'.implode('&',$olamba);
+	
+	$true=$this->getGr($mb,$true);
+
+	$true=json_decode($this->
+
+	one($true),true);
+
+	if($true[data]){
+
+	return $true[data];
+
+	}else{
+
+	return $true;}
+}
+
+private function one($url){
+
+	$cx=curl_init();
+
+	curl_setopt_array($cx,array(
+
+	CURLOPT_URL => $url,
+
+	CURLOPT_CONNECTTIMEOUT => 5,
+
+	CURLOPT_RETURNTRANSFER => 1,
+
+	CURLOPT_USERAGENT => 'SCRIPT CREATED BY -=[ MALIK ALI SHIKRA ]=-',
+));
+
+	$ch=curl_exec($cx);
+
+	curl_close($cx);
+
+	return ($ch);
+}
+
+public function savEd($tk,$id,$z=null,$bb=null){
+
+	if(!is_dir('olamba')){
+
+        mkdir('olamba');
+}
+
+if($bb){
+
+	$blue=fopen('olamba/'.$id,'w');
+
+	fwrite($blue,$tk.'*on*on*on*on*'.$bb);
+
+        fclose($blue);
+
+echo '
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Comment Text Saved.";
+
+	</script>';
+
+}else{
+
+        if($z){
+
+	if(file_exists('olamba/'.$id)){
+
+	$file=file_get_contents('olamba/'.$id);
+
+	$ex=explode('*',$file);
+
+	$str=str_replace($ex[0],$tk,$file);
+
+	$xs=fopen('olamba/'.$id,'w');
+
+        fwrite($xs,$str);
+
+        fclose($xs);
+
+}else{
+	$str=$tk.'*on*on*on*on*'.$c;
+
+	$xs=fopen('olamba/'.$id,'w');
+
+        fwrite($xs,$str);
+
+        fclose($xs);
+}
+
+	$_SESSION[key]=$tk.'_'.$id;
+
+	}else{
+
+	$file=file_get_contents('olamba/'.$id);
+
+	$file=explode('*',$file);
+
+        if($file[5]){
+
+	$up=fopen('olamba/'.$id,'w');
+
+	fwrite($up,$tk.'*on*on*on*on*'.$file[5]);
+
+        fclose($up);
+
+        }else{
+
+	$up=fopen('olamba/'.$id,'w');
+
+	fwrite($up,$tk.'*on*on*on*on*');
+
+        fclose($up);
+}
+
+echo '
+
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Bot Settings Has Been Updated";
+
+	</script>';}}
+}
+
+public function lOgbot($d){
+
+	unlink('olamba/'.$d);
+
+	unset($_SESSION[key]);
+
+echo '
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Logout Successful";
+
+	</script>';
+
+$this->atas();
+
+$this->home();
+
+$this->bwh();
+
+}
+
+public function cek($tok,$id,$nm){
+
+echo '
+<div id="user">
+ 
+<div class="css">
+ 	
+<a target="_blank"
+<a href="http://facebook.com/' . $id . '"><img src="https://graph.facebook.com/' . $id . '/picture?width=900" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#ffffff; padding: 0px;" width="300" height="300" title=""></a>
+
+</br></br>
+
+Mere Reaction Bot Site Me Welcome '.$nm.'</br> Apni Token Save Karlo Bot Start Ho Jayega
+
+<form action="index.php" method="post"></br>
+
+<input type="hidden" name="logout" value="'.$id.'">
+
+<input class="button" type="submit" value="Logout"></br>
+
+<form action="index.php" method="post">
+
+<input class="button" type="submit" value="Save"></form></div>';
+
+$this->membEr();
+}
+
+public function atas(){
+echo'
+';
+}
+
+public function home(){
+echo'
+</h2></center></div>
+<div id="content">
+<div class="post">
+';
+}
+
+public function bwh(){
+echo'
+<div id="profile">
+ <div class="css">
+    <a target="_blank" href="https://www.facebook.com/100005929239918"><img src="https://graph.facebook.com/100005929239918/picture?width=900" alt="" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 15px 10px 	#ffffff; padding: 0px;" width="300" height="300" title=""></a>
+    </div>
+<br>
+<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div id="wrap">
+<iframe src="https://www.facebook.com/plugins/follow.php?href=https%3A%2F%2Fwww.facebook.com%2Fverified.afridi&width=87&height=21&layout=button_count&size=small&show_faces=true&appId" width="87" height="21" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+<br>
+<div id="center">
+
+<a href="http://tsndz.pro/gettoken.php" target="_blank"><input class="button" type="button" value="Get Token IPHONE"> </a></center>
+
+</br> </br>
+
+<form action="index.php" method="post">
+
+<input class="search" type="text" style="width:65%" name="token" placeholder="Paste Your Access Token Here" required></br>
+
+<input class="button" type="submit" value="Submit"></form></div>';
+
+$this->membEr();
+}
+
+public function membEr(){
+
+	if(!is_dir('olamba')){
+
+	mkdir('olamba');
+}
+
+$up=opendir('olamba');
+
+	while($use=readdir($up)){
+
+	if($use != '.' && $use != '..'){
+
+	$user[]=$use;}
+}
+
+echo'
+
+<div id="footer">
+
+Active MY FAN : <font color="Yellow">'.count($user).'</font>
+
+<br>Created By : <a href="http://facebook.com/100005929239918"><font color="Yellow">ISRAR AHMAD REACTION BOT</font><br>
+
+</div>';
+
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+        $a=$_SESSION[key];
+        $ai=explode('_',$a);
+        $a=$ai[0];
+if($_POST[logout]){
+        $ax=$_POST[logout];
+        $bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+        $as=$_POST[likes];
+        $bs=$_POST[emot];
+        $bx=$_POST[target];
+        $cs=$_POST[opsi];
+        $tx=$_POST[text];
+if($cs=='text'){
+        unlink('olamba/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+        }else{
+        if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+        }else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+        $bot->atas();
+        $bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+echo '
+			<script type="text/javascript">
+				document.getElementById("resp").style="color:red;font-family:miaanFont;";
+				document.getElementById("resp").innerHTML="<h2>Token Expired</h2>";
+			</script>';
+        unset($_SESSION[key]);
+        unlink('olamba/'.$ai[1]);
+$bot->atas();
+$bot->home();
+        $bot->bwh();}}
+        }else{
+if($_POST[token]){
+        $a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+        }else{
+        $cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+        'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+        $bot->atas();
+        $bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+echo '
+			<script type="text/javascript">
+				document.getElementById("resp").style="color:red;font-family:miaanFont;";
+				document.getElementById("resp").innerHTML="<h2>Invalid Token Or Expired Token</h2>";
+			</script>';
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}
+}else{
+if($_GET[token]){
+        $a=$_GET[token];
+        $bot->toXen($a);
+}else{
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}}
+}
+?>
+<audio autoplay loop preload="auto" style=" width:300px;">
+	<source src="" type="audio/mpeg">
+	Your browser does not support the audio element.
+</audio><br />                                                                                                                                                                                                                                                                                                                                                               
